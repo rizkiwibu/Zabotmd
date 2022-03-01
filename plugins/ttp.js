b52f47651520819601560995c3fdf0a12ea53043
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-03-01 19:17:39 +0700

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,6 +1,12 @@
+const { sticker5 } = require('../lib/sticker')
+const fs = require('fs')
 let handler = async (m, {conn, text}) => {
   if (!text) throw ('mana teksnya')
-  api = await conn.sendFile(m.chat, `https://viko-api.herokuapp.com/api/maker/ttp?apikey=rxking&text=${text}`, 'ttpp.webp', '', m)
+  buf = await conn.getBuffer(`https://viko-api.herokuapp.com/api/maker/ttp?apikey=rxking&text=${text}`)
+  await fs.writeFileSync('./ttp.webp', buf)
+  api = await sticker5('./ttp.webp', '', packname, author)
+  conn.sendFile(m.chat, api, '', '', m)
+  await fs.unlinkSync('./ttp.webp')
 }
 
 handler.help = ['ttp'].map(v => v + ' <teks>')
