7d169b3c0b8b0e9d868b30ab0f3d806255cc9898
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-03-02 23:06:07 +0700

    creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ZaBot-MD;;;\nFN:ZaBot-MD\nitem1.TEL;waid=6281227144597:+62 812-2714-4597\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:ZaBot-MD\nEND:VCARD"
+                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ikyBot-MD;;;\nFN:rizkkBot-MD\nitem1.TEL;waid=6283817161861:+62 838 1716 1861\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:ikyBot-MD\nEND:VCARD"
                 conn.sendMessage(m.chat, { contacts: { contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
